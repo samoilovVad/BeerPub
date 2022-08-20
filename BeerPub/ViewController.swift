a89057a66c745b20d08da79ae4cc67f671e50ec8
Vadim Samoilov <vadimsamoilov@MacBook-Air-Vadim.local>
2022-08-20 17:39:11 +0300

    add changing in storyboard and some logic to VC

--- BeerPub/ViewController.swift
+++ BeerPub/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelFourth: UILabel!
+    @IBOutlet weak var labelThird: UILabel!
+    @IBOutlet weak var labelSecond: UILabel!
+    @IBOutlet weak var labelFirst: UILabel!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var checkRemainder: UIButton!
+    
+    @IBOutlet weak var buyFirst: UIButton!
+    @IBOutlet weak var buySecond: UIButton!
+    
+    @IBOutlet weak var buyThird: UIButton!
+    
+    @IBOutlet weak var buyFourth: UIButton!
+    
+    @IBOutlet weak var revenueButton: UIButton!
+    
+    @IBOutlet weak var newShiftButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func infoTapped(_ sender: Any) {
+    }
+    @IBAction func checkRemainderTapped(_ sender: Any) {
+    }
+    @IBAction func buyFirstTapped(_ sender: Any) {
+    }
+    @IBAction func buySecondTapped(_ sender: Any) {
+    }
+    @IBAction func buyThirdTapped(_ sender: Any) {
+    }
+    @IBAction func buyFourthTapped(_ sender: Any) {
+    }
+    @IBAction func revenueTapped(_ sender: Any) {
+    }
+    @IBAction func newShiftTapped(_ sender: Any) {
+    }
+    
 }
+
 
