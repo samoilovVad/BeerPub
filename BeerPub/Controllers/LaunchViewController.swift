af553d9f4e0259c111f6d5a324eafc83b89851b0
Vadim Samoilov <vadimsamoilov@MacBook-Air-Vadim.local>
2022-08-21 15:14:06 +0300

    add launch screen

---
+++ BeerPub/Controllers/LaunchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchViewController.swift
+//  BeerPub
+//
+//  Created by Vadim Samoilov on 21.08.22.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
