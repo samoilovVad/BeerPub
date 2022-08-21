d2e6b7a65ec59f4da648b9609308648854222b5c
Vadim Samoilov <vadimsamoilov@MacBook-Air-Vadim.local>
2022-08-21 15:57:56 +0300

    finish launch screen ainmation

--- BeerPub/Controllers/LaunchViewController.swift
+++ BeerPub/Controllers/LaunchViewController.swift
@@ -8,10 +8,51 @@
 import UIKit
 
 class LaunchViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        imageView.image = UIImage(named: "logo500")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(imageView)
+        view.backgroundColor = #colorLiteral(red: 0.9228420854, green: 0.854423821, blue: 0.7227374315, alpha: 1)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            self.animate()
+        })
+    }
+    
+    private func animate() {
+        UIView.animate(withDuration: 1, animations: {
+            let size = self.view.frame.size.width * 1.5
+            
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - size
+            
+            self.imageView.frame = CGRect(
+                x: -(diffX/2),
+                y: diffY/2,
+                width: size,
+                height: size
+            )
+        })
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageView.alpha = 0
+        }, completion: { done in
+            if done {
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+                    self.performSegue(withIdentifier: "toSecond", sender: self)
+                })
+            }
+        })
+    }
 }
